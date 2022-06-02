01ce17f48c56999b4ef5207decc13df1b60941b8
Kyle Wasserberger <57879311+kww-22@users.noreply.github.com>
2022-06-02 12:34:14 -0700

    Update irb_compliance_check.bat

--- irb_compliance_check.bat
+++ irb_compliance_check.bat
@@ -1,3 +1,3 @@
 CALL C:\Users\kylew\anaconda3\Scripts\activate
 CALL conda activate v6_pitching_db
-CALL C:\Users\kylew\anaconda3\envs\v6_pitching_db\python.exe C:\Users\kylew\Documents\GitHub\pitching_biomech\irb_check_in\irb_check_in_main.py
+CALL C:\Users\<USER>\anaconda3\envs\v6_pitching_db\python.exe C:\Users\<USER>\Documents\GitHub\pitching_biomech\irb_check_in\irb_check_in_main.py
